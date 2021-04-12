3784daea6a967a6612d3b0c6b85d3acccc46a1a8
OpenSauce <lawleycobhc@gmail.com>
2021-04-12 21:19:50 +0100

    Linked List

--- Data Structures and Algorithms/Linked List/linked_list.go
+++ Data Structures and Algorithms/Linked List/linked_list.go
@@ -14,7 +14,29 @@ type List struct {
 }
 
 func (L *List) Insert(key interface{}) {
+	list := &Node{
+		next: L.head,
+		key:  key,
+	}
+	if L.head != nil {
+		L.head.prev = list
+	}
+	L.head = list
 
+	l := L.head
+	for l.next != nil {
+		l = l.next
+	}
+	L.tail = l
+}
+
+func (l *List) Display() {
+	list := l.head
+	for list != nil {
+		fmt.Printf("%+v ->", list.key)
+		list = list.next
+	}
+	fmt.Println()
 }
 
 func main() {
@@ -26,13 +48,13 @@ func main() {
 	link.Insert(28)
 	link.Insert(36)
 
-	fmt.Println("\n==============================\n")
+	fmt.Println("\n==============================")
 	fmt.Printf("Head: %v\n", link.head.key)
 	fmt.Printf("Tail: %v\n", link.tail.key)
-	//link.Display()
-	fmt.Println("\n==============================\n")
+	link.Display()
+	fmt.Println("\n==============================")
 	fmt.Printf("head: %v\n", link.head.key)
 	fmt.Printf("tail: %v\n", link.tail.key)
 	//link.Reverse()
-	fmt.Println("\n==============================\n")
+	fmt.Println("\n==============================")
 }
