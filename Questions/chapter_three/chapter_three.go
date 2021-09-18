d930a9aa0e8633f59ef842e46277d2e1de31b995
OpenSauce <lawleycobhc@gmail.com>
2021-09-18 23:58:34 +0100

    Refactor

--- Questions/chapter_three/chapter_three.go
+++ Questions/chapter_three/chapter_three.go
@@ -1,11 +1,15 @@
 package chapter_three
 
 func QuestionOne(word string) {
-	//Not a function question
+	/*
+
+	 */
 }
 
 func QuestionTwo(word string) {
-	//Not a function question
+	/*
+
+	 */
 }
 
 func QuestionThree(word string) string {
