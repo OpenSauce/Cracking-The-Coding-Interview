4c6a38396bdfd92f14680ae941438027ac414a8c
OpenSauce <lawleycobhc@gmail.com>
2021-10-31 22:15:05 +0000

    A whole bunch of maths questions

--- Questions/chapter_six/chapter_six.go
+++ Questions/chapter_six/chapter_six.go
@@ -1,1 +1,51 @@
 package chapter_six
+
+/*
+Question One
+
+You increment the number of pills for each bottle, so if you have .7g higher you'd know it was the 7th bottle.
+To differentiate from 17 you'd have to count the sum of pills to work out if it's 1.7g higher than it should be.
+*/
+
+/*
+Question Two
+Basketball
+*/
+
+/*
+Question Three
+
+You'll have 32 of one colour and 30 the other
+Only 31 dominoes, a domino can only do 1 of each
+*/
+
+/*
+Question Four
+
+*/
+
+/*
+Question Five
+Pour 3 out the 5 to get two, empty and put 2 into 3, pour 1 out of the 5
+*/
+
+/*
+Question Six
+Blue Eyed Islander
+*/
+
+/*
+Question Seven
+
+*/
+
+/*
+Question Eight
+Egg Drop Question
+*/
+
+/*
+Question Nine
+
+It is based on the number of factorals, perfectly square numbers have an odd number of factorals which will trigger them to be open.
+*/
--- Questions/chapter_six/chapter_six_test.go
+++ Questions/chapter_six/chapter_six_test.go
@@ -1,16 +1,1 @@
 package chapter_six
-
-/*
-Question One
-
-You increment the number of pills for each bottle, so if you have .7g higher you'd know it was the 7th bottle.
-To differentiate from 17 you'd have to count the sum of pills to work out if it's 1.7g higher than it should be.
-*/
-
-/*
-Question Two
-*/
-
-/*
-Question Two
-*/
