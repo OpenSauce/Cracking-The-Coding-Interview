0ee71b3ec1cc4ad0562fe54c712b25251995879a
OpenSauce <lawleycobhc@gmail.com>
2021-11-02 20:03:10 +0000

    Chapter eight, question one

--- Questions/chapter_eight/chapter_eight.go
+++ Questions/chapter_eight/chapter_eight.go
@@ -1,1 +1,22 @@
 package chapter_eight
+
+func QuestionOne(n int) int {
+	memo := make([]int, n+1)
+	for i, _ := range memo {
+		memo[i] = -1
+	}
+	return countWays(n, memo)
+}
+
+func countWays(n int, memo []int) int {
+	if n < 0 {
+		return 0
+	} else if n == 0 {
+		return 1
+	} else if memo[n] > -1 {
+		return memo[n]
+	} else {
+		memo[n] = countWays(n-1, memo) + countWays(n-2, memo) + countWays(n-3, memo)
+		return memo[n]
+	}
+}
