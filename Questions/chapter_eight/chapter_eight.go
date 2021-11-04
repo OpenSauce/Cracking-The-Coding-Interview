339b9d671bd697a272d2136fa74ebecbb5a6bd53
OpenSauce <lawleycobhc@gmail.com>
2021-11-04 19:16:57 +0000

    Question Two

--- Questions/chapter_eight/chapter_eight.go
+++ Questions/chapter_eight/chapter_eight.go
@@ -20,3 +20,39 @@ func countWays(n int, memo []int) int {
 		return memo[n]
 	}
 }
+
+type point struct {
+	x int
+	y int
+}
+
+func QuestionTwo(grid [][]int) []int {
+	var path []int
+	failedPoints := make(map[point]struct{})
+	if getPath(grid, len(grid[0]), len(grid), path, failedPoints) {
+		return path
+	} else {
+		return nil
+	}
+}
+
+func getPath(grid [][]int, row int, column int, path []int, failedPoints map[point]struct{}) bool {
+	if grid[row][column] == 0 {
+		return false
+	}
+
+	_, ok := failedPoints[point{row, column}]
+	if ok {
+		return false
+	}
+
+	atOrigin := row == 0 && column == 0
+
+	if atOrigin || getPath(grid, row, column-1, path, failedPoints) || getPath(grid, row-1, column, path, failedPoints) {
+		path = append(path, grid[row][column])
+		return true
+	}
+
+	failedPoints[point{row, column}] = struct{}{}
+	return false
+}
