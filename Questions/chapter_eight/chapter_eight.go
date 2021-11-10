409214eb6f517f9ea03194f1e4c114bced3b285d
OpenSauce <lawleycobhc@gmail.com>
2021-11-10 19:32:07 +0000

    Tower of Hanoi

--- Questions/chapter_eight/chapter_eight.go
+++ Questions/chapter_eight/chapter_eight.go
@@ -124,3 +124,48 @@ func multiply(smaller int, bigger int) int {
 		return (halfProd << 1) + bigger
 	}
 }
+
+type Stack struct {
+	top      *StackNode
+	capacity int
+}
+
+type StackNode struct {
+	data interface{}
+	next *StackNode
+}
+
+func (s *Stack) pop() (interface{}, error) {
+	if s.top != nil {
+		data := s.top.data
+		s.top = s.top.next
+		return data, nil
+	}
+	return 0, fmt.Errorf("empty Stack")
+}
+
+func (s *Stack) push(in interface{}) {
+	t := StackNode{data: in, next: s.top}
+	s.top = &t
+}
+
+func (s *Stack) peek() (interface{}, error) {
+	if s.top != nil {
+		return s.top.data, nil
+	}
+	return 0, fmt.Errorf("empty Stack")
+}
+
+func (s *Stack) isEmpty() bool {
+	return s.top == nil
+}
+
+func MoveTo(n int, origin Stack, dest Stack, buffer Stack) {
+	if n == 0 {
+		return
+	}
+	MoveTo(n-1, origin, buffer, dest)
+	val, _ := origin.pop()
+	dest.push(val)
+	MoveTo(n-1, buffer, dest, origin)
+}
